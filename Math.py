31d9de8f4708bdb10495c2a574790611f3808627
mli42 <mli@student.42.fr>
2022-07-04 17:31:41 +0200

    describe done: Math class

---
+++ Math.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+
+class Math:
+
+    @staticmethod
+    def count(df: pd.DataFrame) -> int:
+        return len(df)
+
+    @staticmethod
+    def mean(df: pd.DataFrame) -> float:
+        return Math.sum(df) / Math.count(df)
+
+    @staticmethod
+    def std(df: pd.DataFrame) -> float:
+        res = 0
+        m = Math.count(df)
+        mean = Math.mean(df)
+
+        for x in df:
+            if not np.isnan(x):
+                res += (x - mean) ** 2
+        res = (res / m) ** 0.5
+        return res
+
+    @staticmethod
+    def min(df: pd.DataFrame) -> float:
+        if Math.count(df) == 0:
+            raise ValueError('Empty DataFrame')
+        local_min = df[0]
+        for x in df:
+            if x < local_min:
+                local_min = x
+        return local_min
+
+    @staticmethod
+    def max(df: pd.DataFrame) -> float:
+        if Math.count(df) == 0:
+            raise ValueError('Empty DataFrame')
+        local_max = df[0]
+        for x in df:
+            if x > local_max:
+                local_max = x
+        return local_max
+
+    @staticmethod
+    def quartile(df: pd.DataFrame, n: float) -> float:
+        df = df.sort_values()
+        index: float = np.ceil(Math.count(df) * (n / 4)) - 1
+
+        return df[index]
+
+    @staticmethod
+    def first_quartile(df: pd.DataFrame) -> float:
+        return Math.quartile(df, 1)
+
+    @staticmethod
+    def second_quartile(df: pd.DataFrame) -> float:
+        return Math.quartile(df, 2)
+
+    @staticmethod
+    def third_quartile(df: pd.DataFrame) -> float:
+        return Math.quartile(df, 3)
+
+    @staticmethod
+    def sum(df: pd.DataFrame) -> float:
+        res = 0
+        for x in df:
+            if not np.isnan(x):
+                res += x
+        return res
--- describe.py
+++ describe.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 import argparse
+from Math import Math
 import utils
 
 ROW_INDEX = {
@@ -16,40 +17,29 @@ ROW_INDEX = {
     "Max": 'max',
 }
 
-
-class Describe:
-    def __init__(self, df: pd.DataFrame) -> None:
-        self.count = 0
-        self.mean = 0
-        self.std = 0
-        self.min = 0
-        self.first_quartile = 0
-        self.second_quartile = 0
-        self.third_quartile = 0
-        self.max = 0
-
-
-def describe(df: pd.DataFrame) -> None:
+def describe(df: pd.DataFrame) -> pd.DataFrame:
     numerical_features = df.select_dtypes(include=[np.number])
     if 'Index' in numerical_features.columns:
         numerical_features.drop('Index', axis=1, inplace=True)
 
     describe_data = {}
     for index, data in numerical_features.iteritems():
-        feature_description = Describe(df)
-        describe_data[index] = [getattr(feature_description, key) for key in ROW_INDEX.values()]
-        break
+        describe_data[index] = [getattr(Math, key)(data) for key in ROW_INDEX.values()]
 
     description = pd.DataFrame(describe_data, index=ROW_INDEX.keys())
-    print(description)
+    return description
 
 
 def main():
     parser = argparse.ArgumentParser(description='Train model with linear regression')
     parser.add_argument('dataset', type=str, help='dataset on which "describe" is performed')
     args = parser.parse_args()
+
     df = utils.get_dataset(args.dataset)
-    describe(df)
+    description = describe(df)
+
+    pd.set_option('display.max_columns', None)
+    print(description)
 
 
 if __name__ == "__main__":
