80d4c55abb9c26e5a002bc5c3af7096fb8b9535b
mli42 <mli@student.42.fr>
2022-07-13 11:57:13 +0200

    histogram: delete unused axes + one legend + argparse

--- histogram.py
+++ histogram.py
@@ -3,26 +3,36 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import argparse
 import utils
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Make histogram about houses marks distribution per discipline')
+    args = parser.parse_args()
+
     df = utils.get_dataset('./datasets/train.csv')
     houses = df['Hogwarts House'].unique()
     features = utils.get_numeric_features(df, drop_index=True).columns
 
-    subplot_len = int(len(features) ** .5) + 1
-    fix, axes = plt.subplots(nrows=subplot_len, ncols=subplot_len, gridspec_kw={'hspace': .5, 'wspace': .2})
+    size = int(len(features) ** .5) + 1
+    fig, axes = plt.subplots(nrows=size, ncols=size, gridspec_kw={'hspace': .5, 'wspace': .2})
     for i, feature in enumerate(features):
-        plot = axes[i // subplot_len][i % subplot_len]
+        plot = axes[i // size][i % size]
         plot.set_title(feature)
         plot.set_xlabel('Marks')
         plot.set_ylabel('Nº of students')
+        plot.set_xticks([])
+        plot.set_yticks([])
         for house in houses:
             value = df[df['Hogwarts House'] == house][feature].dropna()
             plot.hist(value, alpha=0.6)
-        plot.legend(houses)
         plot.grid()
+    i += 1
+    while i % size != 0:
+        fig.delaxes(axes[i // size][i % size])
+        i += 1
+    fig.legend(houses, loc='lower right')
     plt.show()
 
 
