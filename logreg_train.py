866746f1b2f9a23a41b271b13483c94e10abb696
mli42 <mli@student.42.fr>
2022-07-22 17:58:54 +0200

    utils: add functions + logreg_train: copied ft_linear_reg

---
+++ logreg_train.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+import utils
+import argparse
+from typing import List
+
+class MyLogRegression():
+
+    def __init__(self, alpha: float, max_iter: int):
+        self.alpha = alpha
+        self.max_iter = max_iter
+        self.theta = utils.get_theta()
+        print(f"MyLR: Using {self.alpha = }, and {self.max_iter = }")
+        print(f"MyLR: got theta {utils.str_array(self.theta)}")
+
+    @staticmethod
+    def cost_(y: np.ndarray, y_hat: np.ndarray) -> float:
+        """Computes the mean squared error of two non-empty numpy.ndarray.
+            The two arrays must have the same dimensions.
+        Args:
+            y: has to be an numpy.ndarray, a vector.
+            y_hat: has to be an numpy.ndarray, a vector.
+        Returns:
+            The mean squared error (MSE) of the two vectors as a float.
+        """
+        if y.shape != y_hat.shape:
+            return None
+        j_elem = (y_hat - y) ** 2 / y.shape[0]
+        return np.sum(j_elem)
+
+    def plot_hypo(self, x: np.ndarray, y: np.ndarray, y_hat: np.ndarray) -> None:
+        plt.figure()
+        # Data repartition
+        plt.plot(x, y, "o")
+        # Prediction model
+        plt.plot(x, y_hat)
+        plt.title('Data repartition and predition model')
+        plt.legend(['Dataset','Hypothesis'])
+        plt.xlabel("Mileage (km)")
+        plt.ylabel("Price of car")
+
+    def plot(self, x: np.ndarray, y: np.ndarray, y_hat: np.ndarray, losses: List[float]) -> None:
+        self.plot_hypo(x, y, y_hat)
+
+        plt.figure()
+        plt.title('Train loss through epochs')
+        plt.plot(losses)
+        plt.legend(['Loss'])
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+
+        plt.show()
+
+    def minmax(self, x: np.ndarray) -> np.ndarray:
+        """Computes the normalized version of a non-empty numpy.ndarray using the min-max standardization.
+        Args:
+            x: has to be an numpy.ndarray, m * 1.
+        Returns:
+            x' as a numpy.ndarray, m * 1.
+        """
+        res = utils.minmax(x, np.min(x), np.max(x))
+        return res
+
+    def gradient(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """Computes a gradient vector from three non-empty numpy.ndarray
+        Args:
+            x: has to be a numpy.ndarray, a matrix of dimension m * 1.
+            y: has to be a numpy.ndarray, a vector of dimension m * 1.
+            theta: has to be a numpy.ndarray, a 2 * 1 vector.
+        Returns:
+            The gradient as a numpy.ndarray, a vector of dimension 2 * 1.
+        """
+        m = x.shape[0]
+        theta0 = self.alpha * np.sum(utils.predict(x, self.theta) - y) / m
+        theta1 = self.alpha * np.sum((utils.predict(x, self.theta) - y) * x) / m
+        nabla_j = np.asarray([[theta0], [theta1]])
+        return nabla_j
+
+    def fit(self, x: np.ndarray, y: np.ndarray, show_gradient: bool) -> None:
+        """
+        Description:
+            Fits the model to the training dataset contained in x and y.
+        Args:
+            x: a vector of dimension m * 1
+            y: a vector of dimension m * 1
+            theta: a vector of dimension 2 * 1.
+            alpha: a float, the learning rate
+            max_iter: an int, the number of iterations done
+        """
+        alpha = self.alpha
+        if x.shape != y.shape or self.theta.shape != (2, 1) or self.max_iter <= 0:
+            return None
+        norm_x = self.minmax(x)
+        losses = []
+        for i in range(self.max_iter):
+            gradient = self.gradient(norm_x, y)
+            self.theta -= gradient
+
+            y_hat = utils.predict(norm_x, self.theta)
+            running_loss = MyLogRegression.cost_(y, y_hat)
+            losses.append(running_loss)
+
+            if show_gradient and i % 100 == 0:
+                self.plot_hypo(x, y, y_hat)
+                plt.show()
+
+        utils.save_theta(self.theta)
+        print(f"Last loss: {losses[-1]}")
+
+        self.plot(x, y, y_hat, losses)
+
+def main():
+    DEFAULT_ALPHA = 0.1
+    DEFAULT_ITER = 1000
+
+    parser = argparse.ArgumentParser(description='Train model with logistic regression')
+    parser.add_argument('--alpha', action='store', default=DEFAULT_ALPHA, type=float,
+        help=f'define learning rate (default: {DEFAULT_ALPHA})')
+    parser.add_argument('--max_iter', action='store', default=DEFAULT_ITER, type=int,
+        help=f'define number of iterations (default: {DEFAULT_ITER})')
+    parser.add_argument('--show', action='store_true',
+        help='display plots during gradient descent')
+    args = parser.parse_args()
+
+    mylr = MyLogRegression(alpha=args.alpha, max_iter=args.max_iter)
+    x, y = utils.get_data("./datasets/train.csv")
+    mylr.fit(x, y, args.show)
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -2,12 +2,60 @@ import numpy as np
 import pandas as pd
 from typing import Tuple
 
+THETA_FILE = "theta.txt"
+FEATURES = ["Astronomy", "Ancient Runes"]
+THETA_SIZE = len(FEATURES) + 1
+LABEL_FEATURE = "Hogwarts House"
+
+
+def add_intercept(x: np.ndarray) -> np.ndarray:
+    """ Adds a column of 1's to the non-empty numpy.ndarray x.
+    Args:
+        x: has to be an numpy.ndarray, a matrix of dimension m * n.
+    Returns:
+        x': a matrix of dimension m * (n + 1).
+    """
+    if not isinstance(x, np.ndarray) or x.size == 0:
+        return None
+    ones = np.ones((x.shape[0], 1))
+    res = np.concatenate((ones, x), axis=1)
+    return res
+
+
+def sigmoid(x: np.ndarray) -> np.ndarray:
+    """ Compute the sigmoid of a vector.
+    Args:
+        x: an numpy.ndarray, a vector
+    Returns:
+        The sigmoid value as a numpy.ndarray.
+        None if x is an empty numpy.ndarray.
+    """
+    if x.size == 0:
+        return None
+    return 1 / (1 + np.exp(-x))
+
+def predict(x: np.ndarray, theta: np.ndarray) -> np.ndarray:
+    """ Computes the vector of prediction y_hat.
+    Args:
+        x: an numpy.ndarray, a vector of dimension m * n.
+        theta: an numpy.ndarray, a vector of dimension (n + 1) * 1.
+    Returns:
+        y_hat as a numpy.ndarray, a vector of dimension m * 1.
+    """
+    if (x.shape[1] + 1) != theta.shape[0]:
+        return None
+    x = add_intercept(x)
+    y_hat = sigmoid(x.dot(theta))
+    return y_hat
+
+
 def get_numeric_features(df: pd.DataFrame, drop_index: bool = False) -> pd.DataFrame:
     df = df.select_dtypes(include=[np.number])
 
     if drop_index and 'Index' in df.columns:
         df.drop('Index', axis=1, inplace=True)
     return df
+
 
 def get_dataset(path: str, drop_index: bool = False) -> pd.DataFrame:
     """Read the path as csv and returns the data
@@ -24,3 +72,56 @@ def get_dataset(path: str, drop_index: bool = False) -> pd.DataFrame:
     if drop_index and 'Index' in df.columns:
         df.drop('Index', axis=1, inplace=True)
     return df
+
+
+def str_array(array: np.ndarray) -> str:
+    return str(array).replace('\n', '')
+
+
+def save_theta(theta: np.ndarray) -> None:
+    print(f"{save_theta.__name__}: saving {str_array(theta)}")
+    try:
+        with open(THETA_FILE, "w") as file:
+            for element in theta:
+                file.write(f"{element[0]}\n")
+    except Exception as e:
+        print(f"{save_theta.__name__} failed: {e}")
+
+
+def get_theta() -> np.ndarray:
+    theta = np.asarray([[.0]] * THETA_SIZE)
+
+    try:
+        tmp_theta = theta.copy()
+        with open(THETA_FILE, "r") as file:
+            for i, line in enumerate(file):
+                if i >= THETA_SIZE:
+                    raise Exception("File too long")
+                tmp_theta[i][0] = float(line.strip())
+                if np.isnan(tmp_theta[i][0]):
+                    raise Exception("Has NaN")
+            if i < THETA_SIZE - 1:
+                raise Exception("File too short")
+        theta = tmp_theta
+    except Exception as e:
+        print(f"File `{THETA_FILE}` corrupted: {e}")
+        save_theta(theta)
+    return theta
+
+
+def get_data(path: str) -> Tuple[np.ndarray, np.ndarray]:
+    """Read the data and returns the data X and the labels Y
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: x , y
+        None on error
+    """
+    print(f"Getting data from file '{path}'")
+    try:
+        df = pd.read_csv(path)
+    except Exception as e:
+        print(f"{get_data.__name__}: {e}")
+        exit(1)
+    x = df[FEATURES].to_numpy()
+    y = df[LABEL_FEATURE].to_numpy().reshape(-1, 1)
+
+    return (x, y)
